4caf4e25482e650a7ca4a84543ad585976e003dd
root <root@jtlee1191.mylabserver.com>
2018-05-14 18:53:48 +0000

    apache update

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,15 +1,23 @@
-class profile::apache {
+class profile::apache(
+  Boolean $default_vhost = false,
+  Hash $apache_vhost_servers,
+) {
   class { '::apache': 
-    default_vhost => true,    
+    default_vhost => $default_vhost,    
   }
+  
+  create_resources(::apache::vhost, $apache_vhost_servers)
 
-  $docroot = "/var/www/${facts['fqdn']}"
 
-  ::apache::vhost { $facts['fqdn'] :
-    port    => 80,
-    docroot => $docroot,
-    before  => File["${docroot}/index.html"],
-  }
+#  $docroot = "/var/www/${facts['fqdn']}"
+
+
+
+#  ::apache::vhost { $facts['fqdn'] :
+#    port    => 80,
+#    docroot => $docroot,
+#    before  => File["${docroot}/index.html"],
+#  }
 
   file { "${docroot}/index.html":
     content => "Test page.  This is a test!!",
